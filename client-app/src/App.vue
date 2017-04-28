364ac888204e11348439ea2492a74eaf264a40cd
Oleksandr_Kariaka <oleksandr.kariaka@dev-pro.net>
2017-04-28 17:22:30 +0300

    add vue-chartjs; add test BarChart component

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <search></search>
+    <bar-chart></bar-chart>
   </div>
 </template>
 
 <script type="text/javascript">
 import Search from './components/Search';
+import BarChart from './components/charts/BarChart';
 
 export default {
   name: 'app',
   components: {
     Search,
+    BarChart,
   },
   data() {
     return {
